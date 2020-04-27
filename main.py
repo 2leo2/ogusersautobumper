3735693b66ecbdbeb607a03df8bd70234f5e9f1e
2leo2 <57715523+2leo2@users.noreply.github.com>
2020-04-27 13:22:50 +0800

    Version 1.2
    closes chrome window while waiting, bump more than 5 threads

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ bumps = 0
 
 def first_time_setup():
     ctypes.windll.kernel32.SetConsoleTitleW(
-        "@2leo Autobumper v1.1 - First Time Setup")
-    print("@2leo Autobumper v1.1 - First Time Setup")
-    threads = input("How many threads do you want to bump? [Max 5]: ")
+        "@2leo Autobumper v1.2 - First Time Setup")
+    print("@2leo Autobumper v1.2 - First Time Setup")
+    threads = input("How many threads do you want to bump?: ")
     for x in range(int(threads)):
         threadurl = input("Thread URL " + str(x + 1) + ": ")
         threadurls.append(threadurl)
@@ -49,24 +49,23 @@ def bump():
     driver.add_cookie(
         {'name': 'mybbuser', 'value': str(ogusersmybbuser)})
     ctypes.windll.kernel32.SetConsoleTitleW(
-        "@2leo Autobumper v1.1")
-    print("@2leo Autobumper v1.1")
+        "@2leo Autobumper v1.2")
+    print("@2leo Autobumper v1.2")
     while True:
-        rounds += 1
         for i in range(int(threads)):
             driver.get(threadurls[i])
             randomno = random.randrange(1, 10000000000000)
             message = "Bumped by Leo's autobumper! " + str(randomno)
             try:
-                messagebox = WebDriverWait(driver, 10).until(
+                messagebox = WebDriverWait(driver, 60).until(
                     EC.element_to_be_clickable(
                         (By.ID, "message"))
                 )
             except TimeoutException:
                 print("Element not interactable")
                 quit()
-            if rounds == 1:
-                messagebox.send_keys(message)
+            messagebox.clear()
+            messagebox.send_keys(message)
             sleep(5)
             driver.find_element_by_xpath(
                 "//*[@id=\"quick_reply_submit\"]").click()
@@ -74,9 +73,14 @@ def bump():
             bumps += 1
             if bumps == int(threads) or bumps % int(threads) == 0:
                 if not bumps == 0 or 1:
+                    driver.close()
                     print("Waiting...")
                     bumps = 0
                     sleep(int(delay))
+                    driver = webdriver.Chrome(executable_path=chromedriver)
+                    driver.get("https://ogusers.com/Leo")
+                    driver.add_cookie(
+                        {'name': 'mybbuser', 'value': str(ogusersmybbuser)})
             else:
                 sleep(60)
 
